d6bd956a7ea125289b2bd8038d8228091499a7de
Rifqy <rifqyirsyad7@gmail.com>
2018-12-12 18:03:18 +0700

    rifqy

---
+++ interview.php
@@ -0,0 +1,225 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Multimedia</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+
+        <link rel="stylesheet" href="assets/css/iconfont.css">
+        <link rel="stylesheet" href="assets/css/slick/slick.css">
+        <link rel="stylesheet" href="assets/css/slick/slick-theme.css">
+        <link rel="stylesheet" href="assets/css/stylesheet.css">
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+        <link rel="stylesheet" href="assets/css/jquery.fancybox.css">
+        <link rel="stylesheet" href="assets/css/bootstrap.css">
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/magnific-popup.css">
+        <!--        <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">-->
+
+
+        <!--For Plugins external css-->
+        <link rel="stylesheet" href="assets/css/plugins.css" />
+
+        <!--Theme custom css -->
+        <link rel="stylesheet" href="assets/css/style.css">
+
+        <!--Theme Responsive css-->
+        <link rel="stylesheet" href="assets/css/responsive.css" />
+
+        <script src="assets/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+    <div class="jumbotron" style="background-image: url(assets/images/background.jpg); margin-top: 0px;width: auto;">
+    <body data-spy="scroll" data-target=".navbar-collapse">
+        <div class="culmn">
+            <header id="main_menu" class="header navbar-fixed-top">            
+                <div class="main_menu_bg">
+                    <div class="container">
+                        <div class="row">
+                            <div class="nave_menu">
+                            <nav class="navbar navbar-default">
+                                    <div class="container-fluid">
+                                        <!-- Brand and toggle get grouped for better mobile display -->
+                                        <div class="navbar-header">
+                                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                                <span class="sr-only">Toggle navigation</span>
+                                                <span class="icon-bar"></span>
+                                                <span class="icon-bar"></span>
+                                                <span class="icon-bar"></span>
+                                            </button>
+                                            <a class="navbar-brand" href="#home">
+                                                <img src="assets/images/logo.png"/>
+                                            </a>
+                                        </div>
+
+                                        <!-- Collect the nav links, forms, and other content for toggling -->
+
+
+
+                                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+                                            <ul class="nav navbar-nav navbar-right">
+                                                <li><a href="index.html">HOME</a></li>
+                                                <li><a href="interview.php">INTERVIEW</a></li>
+                                                <li><a href="video.php">VIDEO</a></li>
+                                                <li><a href="novel.php">NOVEL</a></li>
+                                                <li><a href="carabermain.html">TEKA-TEKI</a></li>
+                                            </ul>
+
+
+                                        </div>
+
+                                    </div>
+                                </nav>
+                            </div>	
+                        </div>
+
+                    </div>
+
+                </div>
+            </header> <!--End of header -->
+
+
+
+
+            <!-- History section -->
+            <section id="history" class="history sections">
+                <div class="container">
+                    <div class="row">
+                        <div class="main_history">
+
+                            <div class="col-sm-6">
+                                <div class="single_history_content">
+                                    <div class="head_title">
+                                        <h2>Interview</h2>
+                                    </div>
+                                    <!-- <iframe src="https://drive.google.com/file/d/1NIy7K6D5aXz89XgIa1YqYHofSxQY4b7U/view" style="width:600px; height:750px;" frameborder="0"></iframe> -->
+
+                                    <!-- <embed src="interview.pdf" type="application/pdf" width="100%" height="600px"></embed> -->
+
+                                    <!-- <iframe src="http://docs.google.com/gview?url=http://example.com/mypdf.pdf&embedded=true" style="width:718px; height:700px;" frameborder="0"></iframe> -->
+
+                                    <!-- <embed src="https://drive.google.com/file/d/1NIy7K6D5aXz89XgIa1YqYHofSxQY4b7U/view" width="800px" height="1000px" /> -->
+
+                                    <iframe class="scribd_iframe_embed" title="Tugas 1 Interview" src="https://www.scribd.com/embeds/393911311/content?start_page=1&view_mode=scroll&access_key=key-79OL1nfceTOwDGxTZEwT&show_recommendations=true" data-auto-height="false" data-aspect-ratio="0.7729220222793488" scrolling="no" id="doc_97813" width="550px" height="750px" frameborder="0"></iframe>
+
+
+                                </div>
+                            </div>
+                            <div class="container">
+                                <div class="col-sm-6">
+                                <div class="single_history_content">
+                                    <div class="head_title">
+                                        <h2></h2>
+                                    </div>
+                                    <div class="col-sm">
+                                    <h3 style="color:black;">&nbsp</h3>
+                                    <p style="color:black;" align="justify">Hasil dari Interview menggambarkan keadaan dari perusahaan MC ABADI dari segi penggunaan Multimedia. Dari hasil interview dari nara sumber di dapat bahwa di MC ABADI menggunakan aplikasi Multimedia (Photoshop dan Corel Draw) dalam melakukan desain produknya seperti desain pada undangan. Kemudian di dapat juga bahwa MC ABADI dalam melakukan desain untuk spanduk khususnya undangan, customer dapat memilih desain yang ada, membuat desain baru ataupun kustomisasi dari desain berasal dari customer. Pada segi masalah yang ada MC ABADI memiliki masalah yaitu Missing Font/Gambar ketika melakukan pengiriman master.</p>
+                                    </div>
+                                </div>
+
+                                </div>
+                            </div>
+<br><br>
+                                 <div class="container">
+                                <div class="col-sm">
+                                <div class="single_history_content">
+                                    <div class="head_title">
+                                        <h2>Forum Grup Disscusion (FGD)</h2>
+                                    </div>
+                                
+                                    <iframe width="1024" height="720"
+                                    src="https://www.youtube.com/embed/4XeqXu8h3YE"></iframe>
+                                      <br>
+                                    <p style="color:black;" align="justify">Video di atas adalah video aktifitas Forum Grup Discussion (FGD) yang dilakukan untuk membahas permasalahan yang muncul di perusahaan MC ABADI. Hasil dari Forum Grup Discussion ini pertama dalam hal masalah Missing Font/Gambar yang muncul ketika melakukan pengiriman terdapat beberapa solusi seperti langsung menghubungi costumer ketika masalah ini terjadi, kemudian pengecekan kembali dari setiap proses agar tidak terjadi masalah ini dan terakhir adalah menangani kompabilitas atas aplikasi yang ada. Masalah kedua adalah tantangan online dimana MC ABADI belum memiliki website atau pelayanan/penjualan online kemudian bagaimana cara agar dapat tetap dapat bertahan atau bersaing dengan pesaing - pesaing yang telah memiliki penjualan online, solusi yang di sarankan yaitu melakukan pembuatan akun sosial media untuk melakukan pemasaran perusahaan dan melakukan inovasi atau keunikan akan layanan atau produk agar <i>stand out</i> dan bisa tetap bisa bersaing. Masalah ketiga adalah keterlambatan pengiriman master karena masalah koneksi internet, solusi yang di sarankan pertama yaitu melakukan kontak langsung dengan costumer untuk menjelaskan dan mengirim master yang ada (segi customer) dan melakukan penggantian provider internet yang cocok dengan daerah perusahaan MC ABADI.</p>
+                                    
+                                </div>
+
+                                </div>
+                            </div>
+
+                           <
+                        </div>
+                    </div><!--End of row -->
+                </div><!--End of container -->
+            </section><!--End of history -->
+
+
+             
+
+            
+
+
+
+             <br><br><br><br><br>
+
+            <!--Footer section-->
+            <section class="footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="main_footer">
+                                <div class="row">
+                                    <div class="col-sm-6 col-xs-12">
+                                        <div class="flowus">
+                                            <a href=""><i class="fa fa-facebook"></i></a>
+                                            <a href=""><i class="fa fa-twitter"></i></a>
+                                            <a href=""><i class="fa fa-google-plus"></i></a>
+                                            <a href=""><i class="fa fa-instagram"></i></a>
+                                            <a href=""><i class="fa fa-youtube"></i></a>
+                                            <a href=""><i class="fa fa-dribbble"></i></a>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-sm-6 col-xs-12">
+                                        <div class="copyright_text">
+                                            <p class=" wow fadeInRight" data-wow-duration="1s">2018. All Rights Reserved</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section><!-- End off footer Section-->
+
+
+
+
+
+
+
+
+
+        </div>
+
+        <!-- START SCROLL TO TOP  -->
+
+        <div class="scrollup">
+            <a href="#"><i class="fa fa-chevron-up"></i></a>
+        </div>
+
+        <script src="assets/js/vendor/jquery-1.11.2.min.js"></script>
+        <script src="assets/js/vendor/bootstrap.min.js"></script>
+
+        <script src="assets/js/jquery.magnific-popup.js"></script>
+        <script src="assets/js/jquery.mixitup.min.js"></script>
+        <script src="assets/js/jquery.easing.1.3.js"></script>
+        <script src="assets/js/jquery.masonry.min.js"></script>
+
+        <!--slick slide js -->
+        <script src="assets/css/slick/slick.js"></script>
+        <script src="assets/css/slick/slick.min.js"></script>
+
+
+        <script src="assets/js/plugins.js"></script>
+        <script src="assets/js/main.js"></script>
+
+    </body>
+</html>
